e3d5f1bfae2616412fc488f1587ed16bfb3abebb
heltonjuniodesouza <heltonjuniosouza@gmail.com>
2024-11-23 20:20:05 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+  dialect: 'postgres',
+  url: 'postgresql://postgres:xYANACQFgUejVzcPfiiTjQJGGmLEMRfQ@autorack.proxy.rlwy.net:42326/railway',
+  define: {
+    timespamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
