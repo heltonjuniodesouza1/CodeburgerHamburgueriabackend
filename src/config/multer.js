db6c64915c3ab7597ae77dfec49ff938bf36bcf3
heltonjuniodesouza <heltonjuniosouza@gmail.com>
2024-11-23 20:40:08 -0300

    ajusteirname

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,4 +1,4 @@
-import { v4 } from 'uuid'
+/**import { v4 } from 'uuid'
 import multer from 'multer'
 import { extname, resolve } from 'path'
 
@@ -9,4 +9,31 @@ export default {
       return callback(null, v4() + extname(file.originalname))
     },
   }),
-}
+}*/
+import multer from "multer";
+import { extname, resolve } from "path";
+import { v4 } from "uuid";
+
+
+
+// config apos type
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+// fim
+
+
+// definindo configuracoes do multer como vai receber onde vai enviar e nome
+// multer vai receber file do storage
+// destino onde vai salvar - __dirname
+//como vai retorna arquivo - usando uuid para não ter conflito com nomes duplicados
+export default {
+  storage: multer.diskStorage({
+    destination: resolve(__dirname, "..", "..", "uploads"),
+    filename: (request, file, callback) => {
+      return callback(null, v4() + extname(file.originalname));
+    },
+  }),
+};
