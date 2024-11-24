6f0dd067d5cd3b003f1273eb40195a2004e816ef
heltonjuniodesouza <heltonjuniosouza@gmail.com>
2024-11-23 21:33:59 -0300

    correcaodeprodutos

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -11,7 +11,7 @@ class Product extends Model {
         url: {
           type: Sequelize.VIRTUAL,
           get() {
-            return `https://codeburgerhamburgueriabackend-production.up.railway.app//product-file/${this.path}`
+            return `https://codeburgerhamburgueriabackend-production.up.railway.app/product-file/${this.path}`
           },
         },
       },
